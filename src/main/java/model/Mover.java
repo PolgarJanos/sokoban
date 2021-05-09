3d2d1f9e6db20c7c2d095f7e02f3364dcdf1e45c
Polgar Janos <polgarjano@gmail.com>
2021-05-09 21:20:28 +0200

    documenting

--- src/main/java/model/Mover.java
+++ src/main/java/model/Mover.java
@@ -1,6 +1,16 @@
 package model;
 
+/**
+ * A class that can move an {@code Entity} on a {@code Table}.
+ */
 public interface Mover {
-
+    /**
+     *Moves a {@code Entity} from  a given position to a given direction on a given table.
+     *
+     * @param position which position will be moved
+     * @param direction in what direction will be moved
+     * @param table where the movement is occurred
+     * @throws cantBeMovedException if the {@code Entity} can be moved.
+     */
     void move(Position position, Direction direction, Table table) throws cantBeMovedException;
 }
