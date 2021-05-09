4a424cc37bf16dd9630794be51ace440efa89ca9
Polgar Janos <polgarjano@gmail.com>
2021-05-09 21:03:03 +0200

    documentation cantBeMovedExeption

--- src/main/java/model/Direction.java
+++ src/main/java/model/Direction.java
@@ -1,7 +1,18 @@
 package model;
 
+/**
+ * Represents the directions.
+ */
 public interface Direction {
+    /**
+     * Return the change in the x coordinate when moving to the direction.
+     * @return the change in the x coordinate when moving to the direction.
+     */
     int getXChange();
+    /**
+     * Return the change in the y coordinate when moving to the direction.
+     * @return the change in the y cordinate when moving to the direction.
+     */
     int getYChange();
 
 }
--- src/main/java/model/cantBeMovedException.java
+++ src/main/java/model/cantBeMovedException.java
@@ -1,8 +1,18 @@
 package model;
 
+/**
+ * Signals that an {@code Entity} can 't be moved.
+ */
+public class cantBeMovedException extends Exception {
+    /**
+     * Constructs an {@code cantBeMovedException} with the specified detail message.
+     *
+     * @param message The detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method)
+     */
 
-public class cantBeMovedException extends  Exception{
-    public cantBeMovedException(String message){
+    public cantBeMovedException(String message) {
         super(message);
     }
+
 }
