deff2ab9cd0ca08e3dbf593dc7f1cc4fbaffd87a
Polgar Janos <polgarjano@gmail.com>
2021-05-14 22:02:57 +0200

    Configuratin

--- src/main/java/model/Configuration.java
+++ src/main/java/model/Configuration.java
@@ -1,11 +1,17 @@
 package model;
 
+import model.entityImpl.asEnum.EntityImpl;
+import model.positionImpl.IntPosition;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class Configuration {
 
     public static int BOARD_SIZE= 5;
     public static List<EntityWrapper> ValuableEntities(){
-        return null;
+        List<EntityWrapper> entities =new LinkedList<>();
+        entities.add(new EntityWrapper(EntityImpl.PLAYER, new IntPosition(1,1)));
+        return entities;
     }
 }
